026d9cfcbba321d9ffe6e11c046b20b1a53d5f4d
MihaiCst <ssunltd@gmail.com>
2021-11-25 22:18:13 +0200

    update1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstrepos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
